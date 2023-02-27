577f44b8cfec5a23445ed882b46bafdf21b1a6f6
PrpgramTraveler <2115655391@qq.com>
2023-02-27 15:13:18 +0800

    2023-02-27

---
+++ fitting.py
@@ -0,0 +1,19 @@
+# 多项式函数的拟合实验
+
+from mxnet import autograd, gluon, nd
+from mxnet.gluon import data as gdata, loss as gloss, nn
+
+# 使用一个三阶多项式函数来来生成该样本的标签
+
+# 训练数据集和测试数据集的样本数都设为 100
+n_train, n_test, true_w, true_b = 100, 100, [1.2, -3.4, 5.6], 5
+
+features = nd.random.normal(shape=(n_train + n_test, 1))
+poly_features = nd.concat(features, nd.power(features, 2), nd.power(features, 3))
+# 三阶多项式
+labels = (true_w[0] * poly_features[:, 0] + true_w[1] * poly_features[:, 1] + true_w[2] * poly_features[:, 2] + true_b)
+# 噪声项服从均值为 0 标准差为 0.1 的正态分布
+labels += nd.random.normal(scale=0.1, shape=labels.shape)
+
+# 生成的数据集的前两个样本
+print(features[:2], poly_features[:2], labels[:2])
