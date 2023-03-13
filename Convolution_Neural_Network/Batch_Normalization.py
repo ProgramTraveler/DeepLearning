b9b675a382c458480f37a11e7e975aaa55ee1365
PrpgramTraveler <2115655391@qq.com>
2023-03-13 15:59:32 +0800

    2023-03-13

--- Convolution_Neural_Network/Batch_Normalization.py
+++ Convolution_Neural_Network/Batch_Normalization.py
@@ -1,4 +1,7 @@
 # 批量归一化
+import sys
+# sys.path.append('/root/Deep_Pro/')
+sys.path.append('/home/cowa/wjm/Deep_Pro')
 from mxnet import autograd, gluon, init, nd
 from mxnet.gluon import nn
 from d2lzh import Try_Gpu as tg, Load_Data_Fashion_Mnist as ld, Train_Ch5 as tc5
@@ -19,8 +22,8 @@ def batch_norm(X, gamma, beta, moving_mean, moving_var, eps, momentum):
         else:
             # 使用二维卷积层的情况 计算通道维上 (axis=1) 的均值和方差
             # 这里我们需要保持 X 的形状以便后面可以做广播运算
-            mean = X.mean(axis=(0, 2, 3), keepims=True)
-            var = ((X - mean) ** 2).mean(axis=(0, 2, 3), keepims=True)
+            mean = X.mean(axis=(0, 2, 3), keepdims=True)
+            var = ((X - mean) ** 2).mean(axis=(0, 2, 3), keepdims=True)
         
         # 训练模式下用当前的均值和方差做标准化
         X_hat = (X - mean) / nd.sqrt(var + eps)
@@ -50,17 +53,17 @@ class BatchNorm(nn.Block):
         self.moving_mean = nd.zeros(shape)
         self.moving_var = nd.zeros(shape)
         
-        def forward(self, X):
-            # 如果 X 不在内存上 将 moving_mean 和 moving_var 复制到 X 所在显存上
-            if self.moving_mean.context != X.context:
-                self.moving_mean = self.moving_mean.copyto(X.context)
-                self.moving_var = self.moving_var.copyto(X.context)
-            # 保存更新过的 moving_mean 和 moving_var
-            Y, self.moving_mean, self.moving_var = batch_norm(X, self.gamma.data(), self.beta.data(),
-                                                              self.moving_mean, self.moving_var, eps=1e-5,
-                                                              momentum=0.9)
+    def forward(self, X):
+        # 如果 X 不在内存上 将 moving_mean 和 moving_var 复制到 X 所在显存上
+        if self.moving_mean.context != X.context:
+            self.moving_mean = self.moving_mean.copyto(X.context)
+            self.moving_var = self.moving_var.copyto(X.context)
+        # 保存更新过的 moving_mean 和 moving_var
+        Y, self.moving_mean, self.moving_var = batch_norm(X, self.gamma.data(), self.beta.data(),
+                                                          self.moving_mean, self.moving_var, eps=1e-5,
+                                                          momentum=0.9)
             
-            return Y
+        return Y
         
         
 # 使用批量归一化层的 LeNet
@@ -92,18 +95,18 @@ tc5.train_ch5(net, train_iter, test_iter, batch_size, trainer, ctx, num_epochs)
 # 简洁实现
 net = nn.Sequential()
 net.add(nn.Conv2D(6, kernel_size=5),
-        nn.BatchNorm(6, num_dims=4),
+        nn.BatchNorm(),
         nn.Activation('sigmoid'),
         nn.MaxPool2D(pool_size=2, strides=2),
         nn.Conv2D(16, kernel_size=5),
-        nn.BatchNorm(16, num_dims=4),
+        nn.BatchNorm(),
         nn.Activation('sigmoid'),
         nn.MaxPool2D(pool_size=2, strides=2),
         nn.Dense(120),
-        nn.BatchNorm(120, num_dims=2),
+        nn.BatchNorm(),
         nn.Activation('sigmoid'),
         nn.Dense(84),
-        nn.BatchNorm(84, num_dims=2),
+        nn.BatchNorm(),
         nn.Activation('sigmoid'),
         nn.Dense(10))
 
