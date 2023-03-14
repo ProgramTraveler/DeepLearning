f40a12c6e1e53f711260d7c365f3db419b20df24
PrpgramTraveler <2115655391@qq.com>
2023-03-14 16:58:44 +0800

    2023-03-14

--- demo.py
+++ demo.py
@@ -2,13 +2,9 @@
 from mxnet import nd
 from mxnet.gluon import nn
 
-x = nd.arange(16).reshape(1, 1, 4 , 4)
+def fun() :
+    return 1, 2
 
-print(x.shape)
-print(x)
-# x.reshape((4, 4))
-x[0][0][1, 1] = 18
-print(x)
 
-pool2d = nn.MaxPool2D(3, padding=1, strides=2)
-print(pool2d(x))
+x = fun()
+print(*x)
